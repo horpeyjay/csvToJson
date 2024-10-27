8cac18f0a9d93bd447d819a7752a46cd0d2488b1
Horpe <oniopeyemifrancis@gmail.com>
2024-10-27 22:53:55 +0100

    db.json created

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import csv
 import json
 
-file = open("sample data.csv", 'r')
+file = open("Frequent_Flier_Cleansed.csv", 'r')
 reader = csv.reader(file)
 
 fieldNames = next(reader)
-print (fieldNames)
+#print (fieldNames)
 
 data = []
 
 for row in reader:
     data.append(dict(zip(fieldNames, row)))
 
-print(data)
+#print(data)
 
 #call the json method
 json_data = json.dumps(data)
 
 #open the file and write to it
-json_file = open("FFH.json", 'w')
+json_file = open("db.json", 'w')
 json_file.write(json_data)
 
 #close both files
